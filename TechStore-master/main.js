9b906d1de485014694e72b034fc12d7ad742954c
alican92 <75602059+alican92@users.noreply.github.com>
2021-11-22 12:53:00 -0600

    Init Commit

---
+++ TechStore-master/main.js
@@ -0,0 +1,89 @@
+var listOfProducts;
+var shoppingcart = [];
+
+/** Get products from the json file and store it in a gobal variable */
+function loadProducts() {
+    fetch("./products.json")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(products) {
+        listOfProducts = products;
+        addProductsToWebpage();
+    });
+}
+
+//Onload function for displaying phones.
+function initSite() {
+    updateCartCounter();
+    loadProducts();
+}
+
+/** Uses the loaded products data to create a visible product list on the website */
+function addProductsToWebpage() {
+        var productDivs = document.createElement("div");
+        productDivs.className = "productContainers";
+        for (var i = 0; i < listOfProducts.length; i++) {
+            var productInfo = createProductInfo(listOfProducts[i]);
+            productDivs.appendChild(productInfo);      
+        }
+        
+        document.querySelector("main").appendChild(productDivs);
+        
+}
+//Counts and shows the number of items in cart.
+function updateCartCounter(){
+    document.getElementById("numberOfProducts").innerHTML = shoppingcart.length;
+}
+
+//Creating each individual phone.
+function createProductInfo(product) {
+    var productInfoContainer = document.createElement("div");
+    productInfoContainer.className = "productInfoContainer";
+    
+    var getProductTitle = document.createElement("h1");
+    getProductTitle.innerText = product.title;
+    productInfoContainer.appendChild(getProductTitle);
+    
+    var getProductDescription = document.createElement("p");
+    getProductDescription.innerText = product.description;
+    productInfoContainer.appendChild(getProductDescription);
+    
+    var getProductImage = document.createElement("img");
+    getProductImage.src = "./assets/" + product.image;
+    productInfoContainer.appendChild(getProductImage);
+        
+    var getProductPrice = document.createElement("p");
+    getProductPrice.innerText = product.price + " kr";
+    productInfoContainer.appendChild(getProductPrice);
+    
+    var cartButton = document.createElement("button");
+    cartButton.className = "btn btn-primary";
+    cartButton.setAttribute("id", "addProduct");
+    cartButton.addEventListener("click", function() { 
+        addProduct(product) 
+    });
+    var cartIcon = document.createElement("i");
+    cartIcon.className = "cart2 fas fa-cart-arrow-down";
+    cartButton.appendChild(cartIcon);
+    var buttonText = document.createTextNode(" Lägg till i kundvagnen");
+    cartButton.appendChild(buttonText);
+    productInfoContainer.appendChild(cartButton);
+        
+    return productInfoContainer;
+}
+
+//Parsing localstorage if there is one.
+if (localStorage.shoppingcart) {
+    shoppingcart = JSON.parse(localStorage.shoppingcart);
+}
+
+//Adding phone to cart, array and saves it to localstorage.
+function addProduct(product) {
+    var numberOfProducts = 1;
+    document.getElementById("numberOfProducts").innerHTML = numberOfProducts + shoppingcart.length;
+    shoppingcart.push(product);
+    var shoppingcartString = JSON.stringify(shoppingcart);
+    localStorage.shoppingcart = shoppingcartString;
+    console.log(localStorage.shoppingcart);
+} 
---
+++ TechStore-master/shoppingsitejava.js
@@ -0,0 +1,149 @@
+var cartItems;
+if (localStorage.shoppingcart) {
+    cartItems = JSON.parse(localStorage.shoppingcart);
+} else {
+    cartItems = [];
+}
+
+
+//Onload function for displaying all of the information.
+function listLoaded() {
+    updateCartCounter();
+    createHeader();
+    cartProducts();
+    createCheckout();
+    updateTotpris();
+
+}
+
+//Function for printing out the phones on the web page.
+function cartProducts() {
+    var productDiv = document.createElement("div");
+    productDiv.className = "allProducts";
+
+    var mainProductAll = document.getElementById("allProducts");
+    for (var i = 0; i < cartItems.length; i++) {
+        var product = createCart(cartItems[i]);
+        productDiv.appendChild(product);
+    }
+    mainProductAll.appendChild(productDiv);
+
+}
+
+//Creating number of items in cart.
+function updateCartCounter() {
+    document.getElementById("numberOfProducts").innerHTML = cartItems.length;
+}
+
+//Function for creating the header.
+function createHeader() {
+
+    var headerDiv = document.createElement("div");
+    headerDiv.className = "headerDiv";
+    var header = document.createElement("h1");
+    var headerText = document.createTextNode(" Kundvagn");
+    header.className = "cartHeader";
+    var cartIcon = document.createElement("i");
+    cartIcon.className = "cart2 fas fa-shopping-cart";
+
+    header.appendChild(cartIcon);
+    header.appendChild(headerText);
+    headerDiv.appendChild(header);
+    document.getElementById("allProducts").appendChild(headerDiv);
+
+}
+
+//Function for creating each product from localstorage.
+function createCart(product) {
+
+    var productContainer = document.createElement("div");
+    productContainer.className = "product";
+    productContainer.setAttribute("value", product.title);
+
+    var productimages = document.createElement("img");
+    productimages.src = "./assets/" + product.image;
+    productContainer.appendChild(productimages);
+
+    var productTitles = document.createElement("h2");
+    productTitles.innerText = product.title;
+    productContainer.appendChild(productTitles);
+
+    var productPrices = document.createElement("h5");
+    productPrices.innerText = product.price + "kr";
+    productContainer.appendChild(productPrices);
+
+    var removeButton = document.createElement("button");
+    removeButton.className = "btn btn-danger";
+    removeButton.onclick = deletePhone.bind(removeButton, product);
+    var removeIcon = document.createElement("i");
+    removeIcon.className = "fas fa-trash-alt";
+    removeButton.appendChild(removeIcon);
+    var buttonText = document.createTextNode(" Ta bort");
+    removeButton.appendChild(buttonText);
+
+    productContainer.appendChild(removeButton);
+
+    return productContainer;
+}
+
+//Function for creating the the totalprice text and printing the button.
+function createCheckout() {
+
+    var checkoutText = document.createElement("p");
+    checkoutText.id = "totalpris";
+    document.getElementById("allProducts").appendChild(checkoutText);
+
+    createPurchaseButton();
+}
+
+//Function for creating the confirm purchase button.
+function createPurchaseButton() {
+    var purchase = document.createElement("button");
+    var purchaseIcon = document.createElement("i");
+    var purchaseText = document.createTextNode(" Slutför ditt köp");
+    purchase.className = "btn btn-primary";
+    purchase.addEventListener("click", function () {
+        purchaseConfirmed()
+    });
+    purchaseIcon.className = "fas fa-check";
+
+    purchase.appendChild(purchaseIcon);
+    purchase.appendChild(purchaseText);
+    document.getElementById("allProducts").appendChild(purchase);
+}
+
+//Function for calculating and displaying the totalprice.
+function updateTotpris() {
+    var total = 0;
+
+    for (var i = 0; i < cartItems.length; i++) {
+        total += cartItems[i].price;
+    }
+    var price = document.getElementById("totalpris")
+    price.innerText = "Totalt pris: " + total + "kr";
+}
+
+//Showing that your purchase has been confirmed when clicking the purchase button.
+function purchaseConfirmed() {
+    alert("Köp bekräftat");
+    var numberZero = document.getElementById("numberOfProducts");
+    numberZero.remove();
+    var productRemove = document.getElementById("allProducts");
+    productRemove.remove();
+    window.localStorage.clear();
+
+}
+
+//Delete the selected phone from the cart and update localstorage.
+function deletePhone(product) {
+    for (var i = 0; i < cartItems.length; i++) {
+        if (product === cartItems[i]) {
+            cartItems.splice(i, 1);
+        }
+    }
+    this.parentNode.remove();
+    updateTotpris();
+    localStorage.shoppingcart = JSON.stringify(cartItems);
+    updateCartCounter();
+}
+
